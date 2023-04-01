3d61aead69e0db4b0175f115faaa283e7d699dbc
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-04-01 16:30:34 +0300

    chore: move version to cfg file

--- setup.py
+++ setup.py
@@ -11,11 +11,6 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="""ckanext-example""",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version="0.0.2",
     description="""""",
     long_description=long_description,
     long_description_content_type="text/markdown",
